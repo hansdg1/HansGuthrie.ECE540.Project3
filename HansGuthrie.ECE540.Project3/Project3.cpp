6b1a51b820e115f4e61be7061c612e9ad53192a5
hansdg1 <hansdg1@gmail.com>
2014-04-16 21:30:57 -0500

    2a done

--- HansGuthrie.ECE540.Project3/Project3.cpp
+++ HansGuthrie.ECE540.Project3/Project3.cpp
@@ -10,7 +10,68 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-void part1( )
+
+///Computes the Multivariable regression of matrix a, matrix b, and the dependent matrix. 
+///Requires input of the size of the matricies as an int
+void MultiVariableRegression( matrix a, matrix b, matrix dependent, int size )
+{
+	matrix In = matrix( size, 3 );
+
+	matrix PIn;
+	matrix p, x;
+	double CoD, sqrootofCoD, CC, temp;
+
+	//create matrix In from Row 3, Row 4, and a lot of 1's.
+	for ( int k = 0; k < size; k++ )
+	{
+		In( k, 0 ) = a( k ); // row 3
+		In( k, 1 ) = b( k ); //row 4
+		In( k, 2 ) = 1.0; //set all of last column to 1's
+	}
+
+	PIn = MatrixPseudoInverse( In );
+	if ( PIn.isValid( ) )
+	{
+		// compute the pseudo inverse and check for valid operation.
+		// Multiply pseudo inverse and dependent (row5), and check for valid operation.
+		p = PIn * dependent;
+		if ( p.isValid( ) )
+		{
+			//prints p
+			PrintMatrix( p );
+
+			x = In * p; //x matrix is the product of In and p
+			if ( x.isValid( ) )
+			{
+				CoD = CoefficientOfDetermination( x, dependent );
+				sqrootofCoD = sqrt( CoD );
+				CC = CorrelationCoefficient( x, dependent );
+				printf( "Correlation of Determination (CoD) = %lg\n", CoD );
+				printf( "Sqrt of CoD = %lg\n", sqrootofCoD );
+				printf( "Correlation Coefficient (CC) = %lg\n", CC );
+
+			}// end of valid x = MtrxVector check
+		} // end of valid p = MtrxVector check.
+	}
+}
+
+void ConfidenceInterval( matrix m, int samplesize )
+{
+	//mean, variance
+	double Mean, Stdev, MeanLow, MeanHigh;
+	int Length = m.high( );
+
+	Mean = ComputeMean( m.AsPointer( ), Length );
+	Stdev = ComputeStdev( m.AsPointer( ), Length, Mean );
+	MeanLow = Mean - 1.64*Stdev / sqrt( (double)samplesize );
+	MeanHigh = Mean + 1.64*Stdev / sqrt( (double)samplesize );
+
+	printf( "Mean: %lf\nStdev: %lf\n", Mean, Stdev );
+	printf( "90%% Confidence of being between %lf and %lf\n\n", MeanLow, MeanHigh );
+}
+
+
+int main( )
 {
 	matrix InputMatrix; //the main matrix that we're reading in
 	matrix row1, row2, row3, row4, row5; //separate matricies for each row
@@ -84,70 +145,26 @@ void part1( )
 	printf( "CC4: %lg \n", CorrelCoeff4 );
 
 	//1b)
-	MultiVariableRegression( row3, row4, row5, row2.high( ) );
 
 	//Row 3 and Row 4 have the highest CC, so perform a multivariable regression on them
+	MultiVariableRegression( row3, row4, row5, numcols );
 
-}
 
-///Computes the Multivariable regression of matrix a, matrix b, and the dependent matrix. 
-///Requires input of the size of the matricies as an int
-void MultiVariableRegression( matrix a, matrix b, matrix dependent, int size )
-{
-	matrix In = matrix( size, 3 );
+	//2a)
+	printf( "\n" );
+	int samplesize = 81;
+	printf( "Row 1 stats:\n" );
+	ConfidenceInterval( row1, samplesize );
+	printf( "Row 2 stats:\n" );
+	ConfidenceInterval( row2, samplesize );
+	printf( "Row 3 stats:\n" );
+	ConfidenceInterval( row3, samplesize );
+	printf( "Row 4 stats:\n" );
+	ConfidenceInterval( row4, samplesize );
 
-	matrix PIn;
-	matrix p, x;
-	double CoD, sqrootofCoD, CC, temp;
+	
 
-	//create matrix In from Row 3, Row 4, and a lot of 1's.
-	for ( int k = 0; k < size; k++ )
-	{
-		In( k, 0 ) = a( k ); // row 3
-		In( k, 1 ) = b( k ); //row 4
-		In( k, 2 ) = 1.0; //set all of last column to 1's
-	}
-
-	PIn = MatrixPseudoInverse( In );
-	if ( PIn.isValid( ) )
-	{
-		// compute the pseudo inverse and check for valid operation.
-		// Multiply pseudo inverse and dependent (row5), and check for valid operation.
-		p = PIn * dependent;
-		if ( p.isValid( ) )
-		{
-			//prints p
-			PrintMatrix( p );
-			
-			x = In * p; //x matrix is the product of In and p
-			if ( x.isValid( ) )
-			{
-				CoD = CoefficientOfDetermination( x, dependent );
-				sqrootofCoD = sqrt( CoD );
-				CC = CorrelationCoefficient( x, dependent );
-				printf( "Correlation of Determination (CoD) = %lg\n", CoD );
-				printf( "Sqrt of CoD = %lg\n", sqrootofCoD );
-				printf( "Correlation Coefficient (CC) = %lg\n", CC );
-				
-			}// end of valid x = MtrxVector check
-		} // end of valid p = MtrxVector check.
-	}
-}
-
-double ConfidenceInterval( matrix m )
-{
-
-	return NULL;
-}
-
-void part2( )
-{
-
-}
-
-void main( )
-{
-	part1( );
 	getchar( );
 
+	return 0;
 }
